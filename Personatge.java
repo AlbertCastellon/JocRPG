21e27d8f718a12ed63c6418584befc93f55c0204
Pepe Sufragio <albertkstyon@gmail.com>
2026-03-09 19:44:56 +0100

    attack and reg methods

--- Personatge.java
+++ Personatge.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 enum Race {
   ORC, ELF, DWARF, HUMAN
 }
@@ -14,16 +16,18 @@ public class Personatge {
   private double inte;
   private double wis;
   private double luck;
+  private ArrayList<Arma> weapons;
+  private Arma equipedWeapon;
 
   public Personatge() {
 
   }
-  public Personatge(String name, Race race, int age, double hp, double mp, double str, double dex, double con, double inte, double wis, double luck) {
+  public Personatge(String name, Race race, int age, double str, double dex, double con, double inte, double wis, double luck) {
     this.name = name;
     this.race = race;
     this.age = age;
-    this.hp = hp;
-    this.mp = mp;
+    this.hp = con*50;
+    this.mp = inte*30;
     this.str = str;
     this.dex = dex;
     this.con = con;
@@ -52,7 +56,7 @@ public class Personatge {
     return age;
   }
 
-  public void setEdat(int age) {
+  public void setAge(int age) {
     this.age = age;
   }
 
@@ -120,5 +124,36 @@ public class Personatge {
     this.luck = luck;
   }
 
+  public void equipWeapon(int index) {
+    this.equipedWeapon = weapons.get(index);
+  }
+
+  public void addWeapon(Arma weapon) {
+    this.weapons.add(weapon);
+  }
+
+  public double attack(boolean magic) {
+    double damage = 0;
+    if(this.equipedWeapon.isMagic()){
+      damage = ((this.equipedWeapon.getPower() * this.inte)/100);
+    }else {
+      damage = this.str * ((1+this.equipedWeapon.getPower())/100);
+    }
+    return damage;
+  }
+
+  public void regenerateHp() {
+    this.hp += this.con*3;
+    if(this.hp > this.con*50){
+      this.hp = this.con*50;
+    }
+  }
+
+  public void regenerateMp() {
+    this.mp += this.wis*2;
+    if(this.mp > this.inte*30){
+      this.mp = this.inte*30;
+    }
+  }
 
 }
