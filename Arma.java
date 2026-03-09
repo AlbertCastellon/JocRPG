f7150dbcd20b169bbf026240065824d8c764f50a
Pepe Sufragio <albertkstyon@gmail.com>
2026-03-09 17:04:00 +0100

    classes atributes

--- Arma.java
+++ Arma.java
@@ -1,3 +1,53 @@
+enum WeaponType {
+    SWORD, AXE, MACE, BOW
+}
+
 public class Arma {
-    
+    private String name;
+    private WeaponType type;
+    private boolean magic;
+    private double power;
+
+    public Arma(String name, WeaponType type, boolean magic, double power) {
+        this.name = name;
+        this.type = type;
+        this.magic = magic;
+        this.power = power;
+    }
+
+    public Arma() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WeaponType getType() {
+        return type;
+    }
+
+    public void setType(WeaponType type) {
+        this.type = type;
+    }
+
+    public boolean isMagic() {
+        return magic;
+    }
+
+    public void setMagic(boolean magic) {
+        this.magic = magic;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
